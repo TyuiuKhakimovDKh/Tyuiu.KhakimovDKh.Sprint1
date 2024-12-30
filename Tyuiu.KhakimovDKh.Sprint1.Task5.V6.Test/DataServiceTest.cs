4c62b14480d547270b6ffa51f0d74ba8cb179d28
Khakimov Doston <Khakimovmma12@gmail.com>
2024-12-31 02:50:09 +0500

    Task6

---
+++ Tyuiu.KhakimovDKh.Sprint1.Task5.V6.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KhakimovDKh.Sprint1.Task5.V6.Lib;
+namespace Tyuiu.KhakimovDKh.Sprint1.Task5.V6.Test
+{
+
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckExpression()
+        {
+            var dataService = new DataService();
+            Assert.AreEqual(7, dataService.Calculate(35));
+        }
+    }
+}
---
+++ Tyuiu.KhakimovDKh.Sprint1.Task6.V13.Test/DataSeviceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KhakimovDKh.Sprint1.Task6.V13.Lib;
+namespace Tyuiu.KhakimovDKh.Sprint1.Task6.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string str = "ABCD";
+            Assert.AreEqual(true, ds.CheckWordsAlphabet(str));
+        }
+    }
+}
